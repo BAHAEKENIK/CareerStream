008a95a3bae24763ef682cfe629e170cc3879dbc
BAHAE KENIKSSI <bahaekenik@gmail.com>
2026-01-21 21:36:51 +0100

    fix: tailwind content paths for vercel

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "D:/CareerStream/frontend/index.html",
-    "D:/CareerStream/frontend/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
